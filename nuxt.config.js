ed524c109fc05ed00524b69f3c40cc9dfca9cc7c
Demo Macro <abc@imst.xyz>
2020-07-04 01:08:11 +0800

    Update site

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,11 +46,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: 'Funish CSS' + ' | ' + process.env.npm_package_description || process.env.npm_package_name,
+    title: 'Funish CSS' + ' | ' + 'A concise front-end CSS framework.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'A concise front-end CSS framework.' }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
