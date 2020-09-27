76fbb333b7acf418fec0e50d33aaccbdee557ad3
Demo Macro <abc@imst.xyz>
2020-09-27 15:47:25 +0800

    Add '.fu-pagination' Class

---
+++ docs/pagination.md
@@ -0,0 +1,21 @@
+# Pagination
+
+Pagination 模块用于创建分页导航
+
+## 用法
+
+使用类选择器 `.fu-pagination` 以应用样式。
+
+```html
+<div class="fu-pagination">
+  <span>Previous</span>
+  <em>1</em>
+  <a href="#url">2</a>
+  <a href="#url">3</a>
+  <span>…</span>
+  <a href="#url">8</a>
+  <a href="#url">9</a>
+  <a href="#url">10</a>
+  <a>Next</a>
+</div>
+```
---
+++ pages/test/pagination.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Height</h1>
+    <nav class="fu-flex fu-main-center">
+      <div class="fu-pagination">
+        <span>Previous</span>
+        <em>1</em>
+        <a href="#url">2</a>
+        <a href="#url">3</a>
+        <span>…</span>
+        <a href="#url">8</a>
+        <a href="#url">9</a>
+        <a href="#url">10</a>
+        <a>Next</a>
+      </div>
+    </nav>
+  </div>
+</template>
